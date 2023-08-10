3ee35dfb79da41f2bca87eb5a1a7578cba13131d
Arturo López <lgzarturo@gmail.com>
2023-08-10 17:59:13 -0500

    feature: commit initial, professional profile

---
+++ README.md
@@ -0,0 +1,40 @@
+# Arturo López - Web Developer
+
+Professional Web Developer &amp; Portfolio Experience
+
+## About me
+
+I am a web developer with experience in the development of web applications, I have knowledge in the development of web applications with the use of the following technologies: HTML, CSS, JavaScript, PHP, MySQL, Laravel, Vue.js, Bootstrap, among others.
+
+## Skills
+
+- HTML
+- CSS
+- JavaScript
+- PHP
+- MySQL
+- Laravel
+- Vue.js
+- Bootstrap
+- Git
+- GitHub
+- Java
+- Kotlin
+- Android Studio
+- Firebase
+- JavaScript
+- React.js
+- Node.js
+- Express.js
+- MongoDB
+- Python
+- Django
+- PostgreSQL
+
+## Contact me
+
+- [LinkedIn](https://www.linkedin.com/in/lgzarturo/)
+- [Twitter](https://twitter.com/lgzarturo)
+- [Instagram](https://www.instagram.com/lgzarturo/)
+- [Facebook](https://www.facebook.com/lgzarturo/)
+- [GitHub](https://github.com/lgzarturo/)
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Professional Web Developer &amp; Portfolio Experience</title>
+</head>
+
+<body>
+    <header>
+        <h1>Arturo López - Web Developer</h1>
+    </header>
+
+    <section>
+        <h2>Web development of web applications</h2>
+        <p>
+            I am a web developer with more than 10 years of experience in the development of web applications, I have experience
+            in the development of web applications with PHP, Laravel, Javascript, VueJS, ReactJS, NodeJS, MySQL, MongoDB, PostgreSQL,
+            HTML5, CSS3, Bootstrap, among others.
+        </p>
+        <p>Cancún, Quintana Roo</p>
+    </section>
+
+    <main>
+        <h3>Services</h3>
+        <section>
+            <h4>Web Applications</h4>
+            <p>
+                I develop web applications with the latest technologies, I have experience in the development of web applications
+                with PHP, Laravel, Javascript, VueJS, ReactJS, NodeJS, MySQL, MongoDB, PostgreSQL, HTML5, CSS3, Bootstrap, among others.
+            </p>
+            <p>
+                I have experience in the development of web applications for different industries such as: real estate, restaurants,
+                hotels, travel agencies, among others.
+            </p>
+        </section>
+        <section>
+            <h4>Mobile Applications</h4>
+            <p>
+                I develop mobile applications with the latest technologies, I have experience in the development of mobile applications
+                with React Native, Android Studio, Java, Kotlin, Swift, among others.
+            </p>
+            <p>
+                Mobile applications are a very important part of any business, since it allows you to reach more customers, I have experience
+                in the development of mobile applications for Android and iOS.
+            </p>
+        </section>
+        <section>
+            <h4>E-commerce</h4>
+            <p>
+                I develop e-commerce with the latest technologies, I have experience in the development email marketing campaigns with
+                Mailchimp, Sendgrid, among others.
+            </p>
+            <p>
+                E-commerce is a very important part of any business, since it allows you to sell your products or services online, i have
+                experience with the most popular payment gateways like PayPal, Stripe, Conekta, MercadoPago, among others.
+            </p>
+        </section>
+    </main>
+
+    <section>
+        <h3>Contact</h3>
+        <p>
+            If you are interested in my services, you can contact me through the following means:
+        </p>
+        <p>
+            <a href="https://www.linkedin.com/in/lgzarturo/" target="_blank">LinkedIn</a>
+            <a href="https://www.facebook.com/lgzarturo/" target="_blank">Facebook</a>
+            <a href="https://www.instagram.com/lgzarturo/" target="_blank">Instagram</a>
+            <a href="https://twitter.com/lgzarturo" target="_blank">Twitter</a>
+            <a href="https://github.com/lgzarturo" target="_blank">GitHub</a>
+        </p>
+        <p>
+            Or you can send me an message to the following contact form:
+        </p>
+        <form action="" method="POST">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" placeholder="Your name" required>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="Your email" required>
+            <label for="message">Message</label>
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="Your message" required></textarea>
+            <p>
+                Please, fill all the fields.
+            </p>
+            <input type="submit" value="Send">
+        </form>
+    </section>
+
+    <footer>
+        <p>Arturo López - Web Developer</p>
+        <p>&copy; 2020 - All rights reserved</p>
+    </footer>
+</body>
+
+</html>
