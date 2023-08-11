87c2e78972b96a2292ca4993400a6c37793d949a
Arturo López <lgzarturo@gmail.com>
2023-08-10 22:44:50 -0500

    fix: :bug: mobile adjusments, format and spacing

--- index.php
+++ index.php
@@ -54,8 +54,9 @@
         </nav>
     </div>
 
-    <div class="background_section py_5">
-        <section id="about" class="container rounded card">
+    <div class="background_section">
+        <div class="overlay"></div>
+        <section id="about" class="container rounded card py_5">
             <h2 class="bb mt_0">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
